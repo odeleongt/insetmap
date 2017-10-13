bc4166bdafbd7599a29c66d3261fb5fca75e773f
Oscar de León <odeleongt@gmail.com>
2017-10-13 07:39:50 -0600

    Add exports from dependencies

---
+++ R/utils.R
@@ -0,0 +1,19 @@
+#' @importFrom magrittr %>%
+#' @export
+magrittr::`%>%`
+
+#' @importFrom sf read_sf
+#' @export
+sf::read_sf
+
+#' @importFrom sf st_transform
+#' @export
+sf::st_transform
+
+#' @importFrom sf st_set_crs
+#' @export
+sf::st_set_crs
+
+#' @importFrom dplyr mutate
+#' @export
+dplyr::mutate
